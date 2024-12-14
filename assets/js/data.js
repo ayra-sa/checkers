27887122ef09b8c5841ff054d372aab3af5f1e11
ayra-sa <rizkyaryas11@gmail.com>
2024-12-14 17:06:37 +0700

    update2

---
+++ assets/js/data.js
@@ -0,0 +1,724 @@
+export const data = [
+  {
+    image: "assets/images/catalog-1.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #01N",
+      description: ["Dompet tipis yang muat banyak kartu"],
+      details: [
+        "dimensi : 18 x 9 cm",
+        "slot kartu : 13",
+        "slot foto : 1",
+        "sekat uang kertas : 2",
+        "tempat koin : --",
+        "resleting : --",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/4VHaErGdvA",
+      tokopedia: "https://tokopedia.link/SyFGN5B0mKb",
+      lazada: "https://s.lazada.co.id/s.r8nfu",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-2.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #02N",
+      description: [
+        "Dompet tipis khusus kartu",
+        "Cocok untuk yang suka simplicity",
+      ],
+      details: [
+        "dimensi : 11 x 9 cm",
+        "slot kartu : 3",
+        "slot foto : 1",
+        "sekat uang kertas : 2",
+        "tempat koin : --",
+        "resleting : --",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/6fM4osBqh3",
+      tokopedia: "https://tokopedia.link/mOhbEeF0mKb",
+      lazada: "https://s.lazada.co.id/s.r8nbb",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-3.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #03N",
+      description: [
+        "Dompet yg cocok untuk sehari hari",
+        "Terdapat ruang yg luas dibalik resleting",
+      ],
+      details: [
+        "dimensi : 18 x 9 cm",
+        "slot kartu : 9",
+        "slot foto : 1",
+        "sekat uang kertas : 3",
+        "tempat koin : yes ( dalam resleting )",
+        "resleting : 1",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/5fTXd3fidO",
+      tokopedia: "https://tokopedia.link/ULWjzcJ0mKb",
+      lazada: "https://s.lazada.co.id/s.r8nLD",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-4.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #05N",
+      description: [
+        "Dompet model simple dgn ruang",
+        "uang kertas yg tebal & lebar",
+      ],
+      details: [
+        "dimensi : 18 x 9 cm",
+        "slot kartu : 6",
+        "slot foto : 1",
+        "sekat uang kertas : 2",
+        "tempat koin : yes ( dalam resleting )",
+        "resleting : 1",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/g4rfty0xO",
+      tokopedia: "https://tokopedia.link/tBjvGWK0mKb",
+      lazada: "https://s.lazada.co.id/s.r8neR",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-5.jpg",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #06N",
+      description: [
+        "Dompet ukuran 3/4 yg banyak fitur",
+        "dan bisa muat extra kapasitas",
+      ],
+      details: [
+        "dimensi : 14 x 11 cm",
+        "slot kartu : 8",
+        "slot foto : 4",
+        "sekat uang kertas : 2",
+        "tempat koin : 1 ( kancing cetet )",
+        "resleting : 2",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/10hjmRA2rD",
+      tokopedia: "https://tokopedia.link/8543JwM0mKb",
+      lazada: "",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-6.jpg",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #07N",
+      description: [
+        "Dompet ukuran 3/4 dengan fitur yang",
+        "lebih simple dibanding #06N",
+      ],
+      details: [
+        "dimensi : 15 x 9.5 cm",
+        "slot kartu : 5",
+        "slot foto : 2",
+        "sekat uang kertas : 1",
+        "tempat koin : 1 ( kancing cetet )",
+        "resleting : --",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/1fxOrltOpm",
+      tokopedia: "https://tokopedia.link/oj4m5TN0mKb",
+      lazada: "",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-7.jpg",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #08N",
+      description: [
+        "Dompet ukuran standar yang",
+        "slick & simple tanpa slot untuk foto",
+      ],
+      details: [
+        "dimensi : 11 x 9 cm",
+        "slot kartu : 6",
+        "slot foto : --",
+        "sekat uang kertas : 1",
+        "tempat koin : --",
+        "resleting : --",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/4puQdc7GnS",
+      tokopedia: "https://tokopedia.link/XjrsARQ0mKb",
+      lazada: "https://s.lazada.co.id/s.r8nTI",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-8.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #09N",
+      description: [
+        "Dompet panjang dengan resleting luar",
+        "supaya terhindar dari isi yg tercecer",
+      ],
+      details: [
+        "dimensi : 18 x 9 cm",
+        "slot kartu : 6",
+        "slot foto : 1",
+        "sekat uang kertas : 2",
+        "tempat koin : yes ( dalam resleting )",
+        "resleting : 1",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/4puQddK46J",
+      tokopedia: "https://tokopedia.link/sPoF7MV0mKb",
+      lazada: "https://s.lazada.co.id/s.r8noV",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-9.jpg",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #10N",
+      description: [
+        "Dompet ukuran standar dgn extra flap",
+        "untuk extra ruang kartu identitas / foto",
+      ],
+      details: [
+        "dimensi : 11 x 9 cm",
+        "slot kartu : 4",
+        "slot foto : 2",
+        "sekat uang kertas : 2",
+        "tempat koin : --",
+        "resleting : --",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/4fb0RLXUk9",
+      tokopedia: "https://tokopedia.link/ruzKSEc1mKb",
+      lazada: "https://s.lazada.co.id/s.r8nOC",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-10.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #11N",
+      description: [
+        "Dompet ukuran panjang yang terlihat",
+        "sangat slick dengan design yg simple",
+      ],
+      details: [
+        "dimensi : 18 x 9 cm",
+        "slot kartu : 8",
+        "slot foto : 1",
+        "sekat uang kertas : 3",
+        "tempat koin : 1 ( kancing cetet )",
+        "resleting : --",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/4KyA2kygo2",
+      tokopedia: "https://tokopedia.link/0DykRke1mKb",
+      lazada: "https://s.lazada.co.id/s.r8ngP",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-11.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #12N",
+      description: [
+        "Dompet ukuran standar design classic",
+        "all time favorit model",
+      ],
+      details: [
+        "dimensi : 11 x 9 cm",
+        "slot kartu : 4",
+        "slot foto : 1",
+        "sekat uang kertas : 2",
+        "tempat koin : --",
+        "resleting : --",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/4VHaF5AmVY",
+      tokopedia: "https://tokopedia.link/EOJEWKf1mKb",
+      lazada: "https://s.lazada.co.id/s.r8nPs",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-12.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #14N",
+      description: [
+        "Dompet ukuran standar model classic",
+        "dengan tambahan resleting",
+      ],
+      details: [
+        "dimensi : 11 x 9 cm",
+        "slot kartu : 4",
+        "slot foto : 1",
+        "sekat uang kertas : 2",
+        "tempat koin : yes ( dalam resleting )",
+        "resleting : 1",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/5KqhEdGD7i",
+      tokopedia: "https://tokopedia.link/rPzjivh1mKb",
+      lazada: "",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-13.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #15N",
+      description: [
+        "Dompet dgn detachable slot tempat ID",
+        "yg bisa dilepas pasang sesuai kebutuhan",
+      ],
+      details: [
+        "dimensi : 11 x 9 cm",
+        "slot kartu : 6",
+        "slot foto : 2 (kancing cetet)",
+        "sekat uang kertas : 2",
+        "tempat koin : yes ( dalam resleting )",
+        "resleting : 1",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/2VWVrRiBxa",
+      tokopedia: "https://tokopedia.link/IxDSp0i1mKb",
+      lazada: "https://s.lazada.co.id/s.r8niE",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-14.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #16N",
+      description: [
+        "Dompet dgn flap 1 slot fotonya  yg bisa",
+        "ditarik keluar untuk bisa dibawa santai",
+      ],
+      details: [
+        "dimensi : 11 x 9 cm",
+        "slot kartu : 4",
+        "slot foto : 2",
+        "sekat uang kertas : 1",
+        "tempat koin : --",
+        "resleting : --",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/pAss8pr6",
+      tokopedia: "https://tokopedia.link/C4Kagqk1mKb",
+      lazada: "https://s.lazada.co.id/s.r8nSS",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-15.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #18N",
+      description: [
+        "Dompet simple dgn tempat kartu",
+        "yg banyak tanpa ruang untuk foto/ ID",
+      ],
+      details: [
+        "dimensi : 11 x 9 cm",
+        "slot kartu : 8",
+        "slot foto : --",
+        "sekat uang kertas : 2",
+        "tempat koin : --",
+        "resleting : --",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/7AIKPmncXW",
+      tokopedia: "https://tokopedia.link/qulluyn1mKb",
+      lazada: "https://s.lazada.co.id/s.r8nRE",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-16.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #25N",
+      description: [
+        "Dompet panjang yang bisa muat banyak",
+        "uang kertas karena sekat uangnya tebal",
+      ],
+      details: [
+        "dimensi : 18 x 9 cm",
+        "slot kartu : 10",
+        "slot foto : 1",
+        "sekat uang kertas : 2",
+        "tempat koin : --",
+        "resleting : --",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/pAsucgiX",
+      tokopedia: "https://tokopedia.link/ihnw8vq1mKb",
+      lazada: "https://s.lazada.co.id/s.r8n5e",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-17.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #38N",
+      description: [
+        "Dompet panajng yg super tipis",
+        "praktis untuk dibawa sehari hari",
+      ],
+      details: [
+        "dimensi : 18 x 9 cm",
+        "slot kartu : 6",
+        "slot foto : 1",
+        "sekat uang kertas : 2",
+        "tempat koin : yes ( dalam resleting )",
+        "resleting : 1",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/8zjzbTQ9qs",
+      tokopedia: "https://tokopedia.link/2UA9Ksx1mKb",
+      lazada: "https://s.lazada.co.id/s.r8n7r",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-18.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #44N",
+      description: [
+        "Dompet panjang dgn lidah berkancing",
+        "fitur lengkap menjadi satu best-seller",
+      ],
+      details: [
+        "dimensi : 18 x 9 cm",
+        "slot kartu : 5",
+        "slot foto : 1",
+        "sekat uang kertas : 5 + 2",
+        "tempat koin : yes ( dalam resleting )",
+        "resleting : 2",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/4fb0RWtJQF",
+      tokopedia: "https://tokopedia.link/Pkx0maA1mKb",
+      lazada: "https://s.lazada.co.id/s.r8n8E",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-19.jpg",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #45N",
+      description: [
+        "Dompet model halaman dgn fitur komplit",
+        "ada tempat simpan koin multifungsinya",
+      ],
+      details: [
+        "dimensi : 11 x 9 cm",
+        "slot kartu : 8",
+        "slot foto : 1",
+        "sekat uang kertas : 1",
+        "tempat koin : 1 ( kancing cetet )",
+        "resleting : --",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/5pmxphJGU9",
+      tokopedia: "https://tokopedia.link/WyvnfsB1mKb",
+      lazada: "https://s.lazada.co.id/s.r8nk9",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-20.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #46N",
+      description: [
+        "Dompet dgn flap samping cocok untuk",
+        "menyimpan foto keluarga / yg disayang",
+      ],
+      details: [
+        "dimensi : 11 x 9 cm",
+        "slot kartu : 7",
+        "slot foto : 2",
+        "sekat uang kertas : 1",
+        "tempat koin : --",
+        "resleting : --",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/5pmxpiRjGo",
+      tokopedia: "https://tokopedia.link/autEBVC1mKb",
+      lazada: "https://s.lazada.co.id/s.r8n9k",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-21.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #50N",
+      description: [
+        "Dompet kartu mini, nyaman dibawa",
+        "dalam kantung celana atau digenggam",
+      ],
+      details: [
+        "dimensi : 10.5 x 8 cm",
+        "slot kartu : 4",
+        "slot foto : --",
+        "sekat uang kertas : 1",
+        "tempat koin : --",
+        "resleting : --",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/8zjzbYO09l",
+      tokopedia: "https://tokopedia.link/eGQLubF1mKb",
+      lazada: "https://s.lazada.co.id/s.r8nZr",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-22.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #61N",
+      description: [
+        "Dompet model halaman yg muat extra",
+        "kartu dan jg uang kertas dalam  resleting",
+      ],
+      details: [
+        "dimensi : 11 x 9 cm",
+        "slot kartu : 9",
+        "slot foto : 1",
+        "sekat uang kertas : 2",
+        "tempat koin : 1 ( dalam resleting )",
+        "resleting : 1",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/1Ve0Nktqj7",
+      tokopedia: "https://tokopedia.link/kKOMpGU1mKb",
+      lazada: "",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-23.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #64N",
+      description: [
+        "Dompet panjang design harmonika",
+        "design unik yg bisa buka di ke2 sisinya",
+      ],
+      details: [
+        "dimensi : 18 x 9 cm",
+        "slot kartu : 11",
+        "slot foto : 2",
+        "sekat uang kertas : 7",
+        "tempat koin : 1 ( dalam resleting )",
+        "resleting : 2",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/3L5eZ9LfDn",
+      tokopedia: "https://tokopedia.link/YphiN3J1mKb",
+      lazada: "https://s.lazada.co.id/s.r8npD",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-24.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #65N",
+      description: [
+        "Dompet panjang sultana dgn space extra",
+        "yg ada ruang privasi dalam resletingnya",
+      ],
+      details: [
+        "dimensi : 18 x 9 cm",
+        "slot kartu : 12 + 8",
+        "slot foto : 1",
+        "sekat uang kertas : 7",
+        "tempat koin : 1 ( dalam resleting )",
+        "resleting : 1",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/8KUIoNGjXX",
+      tokopedia: "https://tokopedia.link/fKfrbyL1mKb",
+      lazada: "https://s.lazada.co.id/s.r8nWs",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-25.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #66N",
+      description: [
+        "Dompet kartu yg bisa menyimpan logam",
+        "mulia dan juga berbagai macam kartu",
+      ],
+      details: [
+        "dimensi : 10.5 x 8 cm",
+        "slot kartu : 20+",
+        "slot foto : 20+",
+        "sekat uang kertas : --",
+        "tempat koin : --",
+        "resleting : --",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/6APoEPbhyn",
+      tokopedia: "https://tokopedia.link/1b5WyMM1mKb",
+      lazada: "https://s.lazada.co.id/s.r8nhb",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-26.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #67N",
+      description: [
+        "Dompet dgn model lipat 3 yg ukuran",
+        "sedikit lbh lebar pada semua ruangannya",
+      ],
+      details: [
+        "dimensi : 12.5 x 10 cm",
+        "slot kartu : 9",
+        "slot foto : 1",
+        "sekat uang kertas : 2",
+        "tempat koin : 1 ( dalam resleting )",
+        "resleting : 1",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/6KjEQjwjll",
+      tokopedia: "https://tokopedia.link/cqMercO1mKb",
+      lazada: "https://s.lazada.co.id/s.r8nQZ",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-27.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #68N",
+      description: [
+        "Dompet ukuran standar yg diutamakan",
+        "untuk bisa muat banyak kartu",
+      ],
+      details: [
+        "dimensi : 11 x 8.5 cm",
+        "slot kartu : 16",
+        "slot foto : --",
+        "sekat uang kertas : 2",
+        "tempat koin : --",
+        "resleting : --",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/1qGp4UUzjB",
+      tokopedia: "https://tokopedia.link/lV7FXwP1mKb",
+      lazada: "https://s.lazada.co.id/s.r8nlD",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-28.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #06AN",
+      description: [
+        "Dompet identik dengan ukuran yg lebih",
+        "sedikit kecil dari CHECKERS #06N",
+      ],
+      details: [
+        "dimensi : 13 x 9 cm",
+        "slot kartu : 8",
+        "slot foto : 4",
+        "sekat uang kertas : 2",
+        "tempat koin : 1 ( kancing cetet )",
+        "resleting : 2",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/AUYp60FevM",
+      tokopedia: "https://tokopedia.link/mn9hMGJeoKb",
+      lazada: "",
+      tiktokshop: "",
+    },
+  },
+  {
+    image: "assets/images/catalog-29.png",
+    altText: "catalog img",
+    content: {
+      title: "CHECKERS #07AN",
+      description: [
+        "Dompet identik dengan ukuran yg lebih",
+        "sedikit kecil dari CHECKERS #07N",
+      ],
+      details: [
+        "dimensi : 13 x 9 cm",
+        "slot kartu : 5",
+        "slot foto : 2",
+        "sekat uang kertas : 1",
+        "tempat koin : 1 ( kancing cetet )",
+        "resleting : --",
+      ],
+    },
+    links: {
+      shopee: "https://s.shopee.co.id/2LD7N3IWpw",
+      tokopedia: "https://tokopedia.link/p119SCLeoKb",
+      lazada: "",
+      tiktokshop: "",
+    },
+  },
+];
---
+++ assets/js/main.js
@@ -0,0 +1,72 @@
+import { data } from "./data.js";
+
+
+const container = document.getElementById("catalog-list");
+
+data.forEach((item,id) => {
+  const catalogCard = document.createElement("div");
+  catalogCard.className = "catalog-card";
+
+  // Gambar
+  const img = document.createElement("img");
+  img.src = item.image;
+  img.alt = item.altText;
+  catalogCard.appendChild(img);
+
+  // Konten
+  const cardContent = document.createElement("div");
+  cardContent.className = "card-content";
+
+  const title = document.createElement("b");
+  title.className = "card-title";
+  title.textContent = `\u300A${id+1}\u300B ${item.content.title}`;
+  cardContent.appendChild(title);
+
+  // Deskripsi
+  item.content.description.forEach((desc) => {
+    const p = document.createElement("p");
+    p.className = "card-desc";
+    p.textContent = desc;
+    cardContent.appendChild(p);
+  });
+
+  // Detail
+  const ul = document.createElement("ul");
+  item.content.details.forEach((detail) => {
+    const li = document.createElement("li");
+    li.textContent = detail;
+    ul.appendChild(li);
+  });
+  cardContent.appendChild(ul);
+  catalogCard.appendChild(cardContent);
+
+  // catalog-divo (Statis)
+  const catalogDivo = document.createElement("div");
+  catalogDivo.className = "catalog-divo";
+
+  const divoTitle = document.createElement("b");
+  divoTitle.textContent = "Dive Deep";
+  catalogDivo.appendChild(divoTitle);
+
+  const divoNote = document.createElement("span");
+  divoNote.textContent = "*more photos / video";
+  catalogDivo.appendChild(divoNote);
+
+  catalogCard.appendChild(catalogDivo);
+
+  // Links
+  const catalogLinks = document.createElement("div");
+  catalogLinks.className = "catalog-link";
+
+  Object.entries(item.links).forEach(([platform, url]) => {
+    const link = document.createElement("a");
+    link.href = url || "#";
+    link.textContent = platform;
+    catalogLinks.appendChild(link);
+  });
+
+  catalogCard.appendChild(catalogLinks);
+
+  // Tambahkan ke container
+  container.appendChild(catalogCard);
+});
